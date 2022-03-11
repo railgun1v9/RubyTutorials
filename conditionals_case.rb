1cd976cba2c18592089d4863f8dd1e504fabf1bb
Misael Barcelona <misael.barcelona@wpengine.com>
2022-03-10 22:25:44 -0500

    Initial Push

---
+++ conditionals_case.rb
@@ -0,0 +1,25 @@
+count = 2
+
+# Case with boolean
+case
+when count == 0
+  puts 'nobody'
+when count == 1
+  puts '1 person'
+when (2..5).include?(count)
+  puts 'several people'
+else
+  puts 'many people'
+end
+
+# Case with value comparison
+case count
+when 0
+  puts 'nobody'
+when 1
+  puts '1 person'
+when (2..5)
+  puts 'several people'
+else
+  puts 'many people'
+end
---
+++ conditionals_unless.rb
@@ -0,0 +1,7 @@
+cart = ['apple', 'banana', 'carrot']
+
+if cart.empty?
+  puts 'The cart is empty.'
+else
+  puts "The first item is: #{cart[0]}"
+end
